8705edd11520caca8d62a70fd3c06556bd1da4a4
mr.Rhenium <mr.rhenium75614@gmail.com>
2023-06-05 06:52:54 +0530

    modify in vercel.json part1 on 04

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ const port = 2804 || process.env.PORT;
 app.use(bodyParser.json());
 
 // cors protection
-// app.use(cors());
+app.use(cors());
 // app.use(corsProtection);
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   if (req.method == "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//     return res.status(200).json({});
+//   }
 
-  next();
-});
+//   next();
+// });
 
 // server static files to the client
 app.use("/", express.static("public"));
